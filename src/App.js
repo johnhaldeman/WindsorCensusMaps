ef84ca98fa4128b27bd3165cbbe26887ac4cab51
John Haldeman <john.haldeman@infoinsightsllc.com>
2017-11-07 18:45:20 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './windsorcensus.css';
+
+class App extends Component {
+  render() {
+    return (
+     <h1>Windsor Census Data Mapped</h1>
+    );
+  }
+}
+
+export default App;
