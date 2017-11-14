ad2f51ab7b3a64099a421f1ea757d2ebe2a2c5ac
John Haldeman <john.haldeman@infoinsightsllc.com>
2017-11-14 18:31:40 -0500

    Add click to zoon

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -17,8 +17,6 @@ config.params = {
   attributionControl: true
 };
 
-
-
 export default class Map extends Component {
 
   constructor(props) {
@@ -27,9 +25,7 @@ export default class Map extends Component {
       map: null,
       tileLayer: null,
       geojsonLayer: null,
-      geojson: null,
-      subwayLinesFilter: '*',
-      numEntrances: null
+      info: null
     };
   }
 
@@ -49,7 +45,7 @@ export default class Map extends Component {
           opacity: 1,
           color: 'black',
           dashArray: '3',
-          fillOpacity: 0.5
+          fillOpacity: 0.7
       };
   }
 
@@ -57,102 +53,66 @@ export default class Map extends Component {
       var layer = e.target;
 
       layer.setStyle({
-          weight: 3,
-          color: '#AAA',
+          weight: 2,
+          color: '#eee',
           dashArray: '',
-          fillOpacity: 0.5
+          fillOpacity: 0.7
       });
 
       if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
           layer.bringToFront();
       }
-      this.info.update(layer.feature.properties);
+      this.state.info.update(layer.feature.properties);
   }
 
   resetHighlight(e) {
-      this.geojson.resetStyle(e.target);
-      this.info.update();
+      this.state.geojsonLayer.resetStyle(e.target);
+      this.state.info.update();
   }
 
   onEachFeature(feature, layer) {
       layer.on({
-          mouseover: this.highlightFeature,
-          mouseout: this.resetHighlight
+          mouseover: this.highlightFeature.bind(this),
+          mouseout: this.resetHighlight.bind(this),
+          click: this.zoomToFeature.bind(this)
       });
   }
 
   init() {
       if (this.state.map) return;
 
-      //let map = L.map(this.mapref).setView([42.17, -82.80], 11);
-
       let map = L.map(this.mapref, config.params);
 
-      // a TileLayer is used as the "basemap"
       const tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.dark',
+        id: 'mapbox.streets',
         accessToken: 'pk.eyJ1Ijoiam9obmhhbGRlbWFuIiwiYSI6ImNqOXFkZnYxcjV6OXkyeHFtMjJneTFmem0ifQ._pha7G_TTw937xciMqPfvw'
       }).addTo(map);
 
-      // set our state to include the tile layer
+      this.addLegend(map);
+      this.addRollovers(map);
+
       this.setState({ map, tileLayer });
   }
 
   addGeoJSONLayer(geojson) {
     const geojsonLayer = L.geoJson(geojson, {
-      onEachFeature: this.onEachFeature,
-      pointToLayer: this.pointToLayer,
-      filter: this.filterFeatures
+      style: this.style.bind(this),
+      onEachFeature: this.onEachFeature.bind(this)
     });
     geojsonLayer.addTo(this.state.map);
     this.setState({ geojsonLayer });
 
-    //this.zoomToFeature(geojsonLayer);
+    this.zoomToFeature(geojsonLayer);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    // code to run when the component receives new props or state
-    // check to see if geojson is stored, map is created, and geojson overlay needs to be added
-    if (this.state.map && !this.state.geojsonLayer) {
-      // add the geojson overlay
-      this.addGeoJSONLayer(getProtoData());
-    }
-  }
-
-  zoomToFeature(target) {
-    // pad fitBounds() so features aren't hidden under the Filter UI element
-    var fitBoundsParams = {
-      paddingTopLeft: [200,10],
-      paddingBottomRight: [10,10]
-    };
-    // set the map's center & zoom so that it fits the geographic extent of the layer
-    this.state.map.fitBounds(target.getBounds(), fitBoundsParams);
-  }
-
-  componentWillUnmount() {
-    // code to run just before unmounting the component
-    // this destroys the Leaflet map object & related event listeners
-    this.state.map.remove();
-  }
-
-  componentDidMount() {
-    if (!this.state.map) this.init();
-
-    /*
-    this.geojson = L.geoJson(getProtoData(), {
-      style: this.style.bind(this),
-      onEachFeature: this.onEachFeature
-    }).addTo(map);
-
-    var legend = L.control({position: 'bottomright'});
+  addLegend(map){
+    let legend = L.control({position: 'bottomright'});
 
     legend.onAdd = function (map) {
         var div = L.DomUtil.create('div', 'info legend'),
             grades = [30, 35, 40, 45, 50]
-        //    labels = [];
 
-        // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
             div.innerHTML +=
                 '<i style="background:' + this.getColor(grades[i] + 1) + '">&nbsp;</i> ' +
@@ -163,26 +123,49 @@ export default class Map extends Component {
     }.bind(this);
 
     legend.addTo(map);
+  }
 
-    this.info = L.control();
+  addRollovers(map){
+    let info = L.control();
 
-    this.info.onAdd = function (map) {
+    info.onAdd = function (map) {
         this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
         this.update();
         return this._div;
     };
 
     // method that we will use to update the control based on feature properties passed
-    this.info.update = function (props) {
-        this._div.innerHTML = (props ? '<h4>Average Age in Tract '+ props.CTUID
-            + '</h4><b>' + props.average_age + '</b> years'
+    info.update = function (featureData) {
+        this._div.innerHTML = (featureData ? '<h4>Average Age in Tract '+ featureData.CTUID
+            + '</h4><b>' + featureData.average_age + '</b> years'
             : '<h4>Average Age in Tract</h4> Hover over a census tract');
     };
 
-    this.info.addTo(map);*/
+    info.addTo(map);
 
+    this.setState({info})
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.map && !this.state.geojsonLayer) {
+      this.addGeoJSONLayer(getProtoData());
+    }
+  }
+
+  zoomToFeature(e) {
+    if(e.target)
+      this.state.map.fitBounds(e.target.getBounds());
+    else
+      this.state.map.fitBounds(e.getBounds());
+  }
+
+  componentWillUnmount() {
+    this.state.map.remove();
+  }
+
+  componentDidMount() {
+    if (!this.state.map) this.init();
+  }
 
   render(){
     return(
